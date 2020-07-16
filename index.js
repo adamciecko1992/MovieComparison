e4c39e3eff187128c20f606a12783e998d8231f0
adamciecko1992 <adam.ciecko@hotmail.com>
2020-07-16 16:54:42 +0200

    https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "e22051a3",
       i: movie.imdbID,
@@ -109,7 +109,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "e22051a3",
         s: searchTerm,
